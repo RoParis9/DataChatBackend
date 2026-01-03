7f490813547a1e68a8e09bc243f7981501be2faa
Rodrigo Paris <rocostaparis@hotmail.com>
2026-01-03 17:28:42 -0300

    chagend the signup input fields

--- src/application/use-cases/auth/signUp/signup.input.ts
+++ src/application/use-cases/auth/signUp/signup.input.ts
@@ -1,6 +1,11 @@
+import { UserRole } from "src/domain/Enums/user-roles"
+
 export interface SignUpInput {
   name: string
   email: string
   password: string
+  isCompanyOwner: boolean
   companyName: string
+  companyId?: string
+  role?: UserRole.DEPARTMENT_MANAGER | UserRole.EMPLOYEE
 }
